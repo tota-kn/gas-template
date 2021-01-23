564cb522d26d7a5bcdaadf8df306eb29a46b2927
iwatos <nonbiriactive@gmail.com>
2021-01-23 19:09:24 +0900

    update

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,27 @@
 function doGet() {
-    console.log('hello typescript')
+    const result = { sample: 'sample' }
+    return returnJson(result)
+}
+
+function doPost() {
+    const result = { sample: 'sample' }
+    return returnJson(result)
+}
+
+function fetchApi(
+    url: string,
+    option: Record<string, unknown>
+): Record<string, unknown> {
+    const apiResult = UrlFetchApp.fetch(url, option)
+    return JSON.parse(apiResult.getContentText())
+}
+
+function returnJson(
+    result: Record<string, unknown>
+): GoogleAppsScript.Content.TextOutput {
+    const data = JSON.stringify(result)
+    const output = ContentService.createTextOutput()
+    output.setMimeType(ContentService.MimeType.JSON)
+    output.setContent(data)
+    return output
 }
